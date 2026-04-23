2846fd5a034bda27d025ffeea786c32512566a61
Karan Kumar <karankumarpawani123@gmail.com>
2026-04-23 13:01:09 +0500

    Enhance task list output formatting
    Added formatting to display the final task list clearly.

--- task_1.py
+++ task_1.py
@@ -3,6 +3,7 @@ items = int(input("how many task you want to enter!"))
 print("Enter items one by one")
 for item in range(items):
     my_task.append(input())
-
+print("\n" + "=" * 40)
+print(f"   Your Final completed task list")
 for i , task in enumerate(my_task):
     print(i , task)
